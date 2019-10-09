126503d19f434d0ad82a9c77854532c3948c3d8f
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-09 18:22:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,11 @@
 def square_array(array)
+  new_array =[]
   counter = 0 
   
   while array[counter] do
     puts array[counter] ** 2 
     counter += 1
+    new_array.push = (array[counter])
   end
   
 end
